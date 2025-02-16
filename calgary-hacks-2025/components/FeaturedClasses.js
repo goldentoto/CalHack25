2ac9a2720fc2ab3096352141055483d7fe357fc9
Toto <iwaoiboaka@gmail.com>
2025-02-15 21:28:30 -0700

    update

--- calgary-hacks-2025/components/FeaturedClasses.js
+++ calgary-hacks-2025/components/FeaturedClasses.js
@@ -12,7 +12,7 @@ export default function FeaturedClasses() {
       description: "Learn authentic pasta making techniques from expert chefs",
       instructor: {
         name: "Chef Maria",
-        image: "https://www.bing.com/images/search?view=detailV2&ccid=w0B38r2R&id=769ABB0D704A30D28340DD16BEF5B96F5C4F149E&thid=OIP.w0B38r2ReM7PGP7R3crhvwHaIE&mediaurl=https%3a%2f%2fspecials-images.forbesimg.com%2fimageserve%2f5ee28b23c8e06b00069a072b%2f960x0.jpg%3ffit%3dscale&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.c34077f2bd9178cecf18fed1ddcae1bf%3frik%3dnhRPXG%252b59b4W3Q%26pid%3dImgRaw%26r%3d0&exph=1045&expw=959&q=Chef+Maria&simid=608025133683116275&FORM=IRPRST&ck=343A1ED78FFE74475F5EE84667E95035&selectedIndex=0&itb=0",
+        image: "/maria.jpg",
       },
       duration: "2h 30m",
     },
@@ -25,7 +25,7 @@ export default function FeaturedClasses() {
       description: "Master the art of sushi making with premium ingredients",
       instructor: {
         name: "Chef Tanaka",
-        image: "https://www.bing.com/images/search?view=detailV2&ccid=50AJ6Uv3&id=C5DBB8064B1F9341CC13EEE1FC530066347CA4F1&thid=OIP.50AJ6Uv3TMgZa7HdFQ4jywHaE7&mediaurl=https%3a%2f%2fwww.viedeluxe.fr%2fwp-content%2fuploads%2f2023%2f01%2fLe_Bristol_Paris_-_Chef_Patissier_Yu_Tanaka_-_CFranck_Juery_-_2_0934.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.e74009e94bf74cc8196bb1dd150e23cb%3frik%3d8aR8NGYAU%252fzh7g%26pid%3dImgRaw%26r%3d0&exph=787&expw=1181&q=Chef+tanaka&simid=607997426862982390&FORM=IRPRST&ck=04A7E4038AE0CE35C2B2EAE6547A63F5&selectedIndex=7&itb=0",
+        image: "/tanaka.jpg",
       },
       duration: "3h",
     },
